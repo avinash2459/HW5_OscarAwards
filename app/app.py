c6ee567c80e07c10bb7c8fd5e08d81ce546db173
Avinash Chamarthy <sc2459@njit.edu>
2021-07-26 16:01:03 -0500

    Task1: Added app and db directories with app.py, init.sql and docker files

---
+++ app/app.py
@@ -0,0 +1,121 @@
+from typing import List, Dict
+import simplejson as json
+from flask import Flask, request, Response, redirect
+from flask import render_template
+from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
+
+app = Flask(__name__)
+
+
+mysql = MySQL(cursorclass=DictCursor)
+
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_DB'] = 'oscarData'
+mysql.init_app(app)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    user = {'username': 'Oscar Project'}
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblOscarImport')
+    result = cursor.fetchall()
+    return render_template('index.html', title='Home', user=user, oscars=result)
+
+
+@app.route('/view/<int:oscar_id>', methods=['GET'])
+def record_view(oscar_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblOscarImport WHERE id=%s', oscar_id)
+    result = cursor.fetchall()
+    return render_template('view.html', title='View Form', oscar=result[0])
+
+
+@app.route('/edit/<int:oscar_id>', methods=['GET'])
+def form_edit_get(oscar_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblOscarImport WHERE id=%s', oscar_id)
+    result = cursor.fetchall()
+    return render_template('edit.html', title='Edit Form', oscar=result[0])
+
+
+@app.route('/edit/<int:oscar_id>', methods=['POST'])
+def form_update_post(oscar_id):
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('actorName'), request.form.get('movieName'), request.form.get('sex'),
+                 request.form.get('age'), request.form.get('year'), oscar_id)
+    sql_update_query = """UPDATE tblOscarImport t SET t.actorName = %s, t.movieName = %s, t.age = %s, t.sex =
+    %s, t.year = %s WHERE t.id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/oscars/new', methods=['GET'])
+def form_insert_get():
+    return render_template('new.html', title='New Oscar Form')
+
+
+@app.route('/oscars/new', methods=['POST'])
+def form_insert_post():
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('actorName'), request.form.get('movieName'), request.form.get('sex'),
+                 request.form.get('age'), request.form.get('year'))
+    sql_insert_query = """INSERT INTO tblOscarImport (actorName,movieName,sex,age,year) VALUES (%s, %s,%s, %s,%s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+@app.route('/delete/<int:oscar_id>', methods=['POST'])
+def form_delete_post(oscar_id):
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM tblOscarImport WHERE id = %s """
+    cursor.execute(sql_delete_query, oscar_id)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/api/v1/oscars', methods=['GET'])
+def api_browse() -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblOscarImport')
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/oscars/<int:oscar_id>', methods=['GET'])
+def api_retrieve(oscar_id) -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM tblOscarImport WHERE id=%s', oscar_id)
+    result = cursor.fetchall()
+    json_result = json.dumps(result);
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/oscars/', methods=['POST'])
+def api_add() -> str:
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/oscars/<int:oscar_id>', methods=['PUT'])
+def api_edit(oscar_id) -> str:
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/oscars/<int:oscar_id>', methods=['DELETE'])
+def api_delete(oscar_id) -> str:
+    resp = Response(status=210, mimetype='application/json')
+    return resp
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
